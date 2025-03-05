3700c69fbaa3966f9a12ae1863c1ac03ad7e1a7a
root <darren@getperformancemarketing.com>
2025-03-05 09:51:22 +0000

    Multer added

---
+++ multer.config.js
@@ -0,0 +1,18 @@
+// multerConfig.js
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Store files in the 'uploads' directory
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const fileExtension = path.extname(file.originalname);
+    cb(null, file.fieldname + '-' + uniqueSuffix + fileExtension); // Rename files to avoid conflicts
+  },
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
